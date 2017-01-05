fc586c57c8170b3aed9c1e6ef3a48ce25f2e34b6
markhorvath <markhorvath7@gmail.com>
2017-01-05 15:01:21 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -26,25 +26,56 @@ var cats = [
         }];
 
 
-
-var ulList = document.getElementById('catlist');
-
-var view = {
-        renderList : function() {
+var viewList = {
+        render : function() {
             for(var i = 0; i < cats.length; i++) {
                 var list = document.createElement('li');
                 list.innerHTML = cats[i].name;
-                ulList.appendChild(list);
+                document.getElementById('catlist').appendChild(list);
                 }
-        },
-        renderDetails : function(i) {
+            },
+        update : function() {
+            var list = $('li');
+            for (i = 0; i < list.length; i++){
+                list[i].addEventListener('click', function(i){
+                var name = document.getElementById('catname');
+                name.innerHTML = octopus.getCatName(i);
+                })
+            }
+        }
+}
+
+var viewDetails = {
+        render : function(i) {
             var elem = document.getElementById('display');
             var name = document.getElementById('catname');
             var pic = document.getElementById('catpic');
+            var clicks = document.getElementById('clicks');
             name.innerHTML = cats[i].name;
             pic.src = cats[i].pic;
+            clicks.innerHTML = "Clicks: " + cats[i].count;
             }
-        }
+}
 
-view.renderList();
-view.renderDetails(0);
+var octopus = {
+        init : function() {
+            viewList.render();
+            viewDetails.render(0);
+            viewList.update();
+        },
+
+        getCatName : function(i){
+            return cats[i].name;
+        },
+
+            clickTrack : function(){
+                var pic = document.getElementById('catpic');
+                var p = document.getElementById('clicks');
+                pic.addEventListener('click', function(){
+                    cats[0].count += 1;
+                    p.innerHTML = "clicks: " + cats[0].count;
+                })
+            }
+}
+
+octopus.init();
